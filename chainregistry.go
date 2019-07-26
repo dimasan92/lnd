d9213a5f9a15b4128c59d9a50094eb791facf54e
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-07-26 14:30:57 +0100

    lnd: expose user agent name as config option
    Expose the neutrino `UserAgentName` conffig option. This can be set by
    starting lnd with the `--neutrino.useragentname=` flag.

--- chainregistry.go
+++ chainregistry.go
@@ -782,6 +782,7 @@ func initNeutrinoBackend(chainDir string) (*neutrino.ChainService, func(), error
 
 	neutrino.MaxPeers = 8
 	neutrino.BanDuration = time.Hour * 48
+	neutrino.UserAgentName = cfg.NeutrinoMode.UserAgentName
 
 	neutrinoCS, err := neutrino.NewChainService(config)
 	if err != nil {
